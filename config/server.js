fbdde949333acc17844059deba2d0e33e85d1cae
Vivek Mehta <vivek2.mehta@ril.com>
2021-05-12 19:30:27 +0530

    Full Project

---
+++ config/server.js
@@ -0,0 +1,5 @@
+module.exports = {
+  APPLICATION_ID: process.env.APPLICATION_ID || 'UCdjarEMOVOhn0ChN9TVMQzJEi9p42BHtiThTh1NkcY',
+  SECRET: process.env.SECRET || 'IDIKq1GgYPs6nz8B-LnuqaR-eKTR_UfzYM4LTu28krg',
+  CALLBACK_URL: process.env.CALLBACK_URL || 'http://localhost:3000'
+};
